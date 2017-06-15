247558a21760d1b609f6a838029bdc6ac0645211
Hasan Kartal <hasankartal18@gmail.com>
2017-06-15 10:04:06 +0300

    003_overview.pdf eklendi. Kod uzerinde değişiklik yapıldı.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+/*
+ * Username : hasankartal
+ * Password : mugiwaraluffy20Ace
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ * Answer :
+*/
 public class Main {
 
     public static void main(String[] args) {
